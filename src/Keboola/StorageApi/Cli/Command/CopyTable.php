8981513b276c6828736a9dc0782d9254fdcf38db
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2013-09-08 18:08:37 -0700

    fixed typo in copy-bucket command

--- src/Keboola/StorageApi/Cli/Command/CopyTable.php
+++ src/Keboola/StorageApi/Cli/Command/CopyTable.php
@@ -27,7 +27,7 @@ class CopyTable extends Command {
 			->setDefinition(array(
 				new InputArgument('sourceTableId', InputArgument::REQUIRED, "source table"),
 				new InputArgument('destinationTableId', InputArgument::REQUIRED, "destination table"),
-				new InputArgument('dstToken', null, InputOption::OPTIONAL, "Destination Storage API Token")
+				new InputArgument('dstToken', null, InputArgument::OPTIONAL, "Destination Storage API Token")
 			));
 	}
 
