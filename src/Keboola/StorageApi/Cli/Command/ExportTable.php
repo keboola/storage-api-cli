4b7b152a30d216d11799f4345013e5070b699807
Martin Halamíček <martin@halamicek.cz>
2015-02-06 13:42:27 +0100

    feat: export async and allow gzip

--- src/Keboola/StorageApi/Cli/Command/ExportTable.php
+++ src/Keboola/StorageApi/Cli/Command/ExportTable.php
@@ -10,6 +10,7 @@
 namespace Keboola\StorageApi\Cli\Command;
 
 use Keboola\Csv\CsvFile;
+use Keboola\StorageApi\TableExporter;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface,
 	Symfony\Component\Console\Output\OutputInterface;
@@ -27,6 +28,7 @@ class ExportTable extends Command {
 				new InputArgument('tableId', InputArgument::REQUIRED, "target table"),
 				new InputArgument('filePath', InputArgument::REQUIRED, "export csv file"),
 				new InputOption('format', 'f', InputOption::VALUE_REQUIRED, "output format", "rfc"),
+				new InputOption('gzip', 'g', InputOption::VALUE_NONE, "gzip file")
 			));
 	}
 
@@ -40,9 +42,18 @@ class ExportTable extends Command {
 		$output->writeln("Table found ok");
 
 		$startTime = time();
-		$sapiClient->exportTable($input-> getArgument('tableId'), $input->getArgument('filePath'), array(
-			'format' => $input->getOption('format'),
-		));
+
+		$exporter = new TableExporter($sapiClient);
+
+		$exporter->exportTable(
+			$input->getArgument('tableId'),
+			$input->getArgument('filePath'),
+			array(
+				'format' => $input->getOption('format'),
+				'gzip' => $input->getOption('gzip'),
+			)
+		);
+
 		$duration = time() - $startTime;
 
 		$output->writeln("Export done in $duration secs.");
