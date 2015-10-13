801e022a9dcff651f3cd26680d75c89998fb6585
Martin Halamíček <martin@halamicek.cz>
2015-10-13 10:44:46 +0200

    feat(project-backup): don't merge sliced files

--- src/Keboola/StorageApi/Cli/Command/BackupProject.php
+++ src/Keboola/StorageApi/Cli/Command/BackupProject.php
@@ -13,11 +13,13 @@ use Aws\S3\S3Client;
 use Keboola\Csv\CsvFile;
 use Keboola\StorageApi\Components;
 use Keboola\StorageApi\Options\Components\ListConfigurationsOptions;
+use Keboola\StorageApi\Options\GetFileOptions;
 use Keboola\StorageApi\TableExporter;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface,
 	Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Filesystem\Filesystem;
 
 
 class BackupProject extends Command {
@@ -45,7 +47,6 @@ class BackupProject extends Command {
 
 		$sapiClient = $this->getSapiClient();
 		$components = new Components($sapiClient);
-		$tableExporter = new TableExporter($sapiClient);
 
 		$tables =  $sapiClient->listTables(null, [
 			'include' => 'attributes,columns,buckets'
@@ -77,25 +78,74 @@ class BackupProject extends Command {
 
 		$tablesCount = count($tables);
 		$onlyStructure = $input->getOption('structure-only');
+		usort($tables, function($a, $b) {
+			return strcmp($a["id"], $b["id"]);
+		});
 		foreach (array_values($tables) as $i => $table) {
 			$output->write($this->format("Table $i/$tablesCount - {$table['id']}"));
-			$tmpFile = $this->getTmpDir() . "/" . uniqid('table');
 			if ($onlyStructure && $table['bucket']['stage'] !== 'sys') {
 				$output->writeln('<comment>Skipped (not sys table)</comment>');
 			} else if (!$table['isAlias']) {
-				$tableExporter->exportTable($table['id'], $tmpFile, [
-					'gzip' => true,
-				]);
-				$s3->putObject([
-					'Bucket' => $bucket,
-					'Key' => $basePath . str_replace('.', '/', $table['id']) . '.csv.gz',
-					'Body' => fopen($tmpFile, 'r'),
-				]);
-				unlink($tmpFile);
+				$this->exportTable($table['id'], $s3, $bucket, $basePath);
 				$output->writeln($this->check());
 			} else {
 				$output->writeln('<comment>Skipped (alias table)</comment>');
 			}
+		}
+	}
+
+	private function exportTable($tableId, S3Client $targetS3, $targetBucket, $targetBasePath)
+	{
+		$client = $this->getSapiClient();
+		$fileId = $client->exportTableAsync($tableId, [
+			'gzip' => true,
+		]);
+		$fileInfo = $client->getFile($fileId["file"]["id"], (new GetFileOptions())->setFederationToken(true));
+
+		// Initialize S3Client with credentials from Storage API
+		$s3Client = S3Client::factory(array(
+			"key" => $fileInfo["credentials"]["AccessKeyId"],
+			"secret" => $fileInfo["credentials"]["SecretAccessKey"],
+			"token" => $fileInfo["credentials"]["SessionToken"]
+		));
+
+
+		$fs = new Filesystem();
+		if ($fileInfo['isSliced'] === true) {
+			// Download manifest with all sliced files
+			$manifest = json_decode(file_get_contents($fileInfo["url"]), true);
+
+			// Download all slices
+			$tmpFilePath = $this->getTmpDir() . '/' . uniqid('sapi-export-');
+			foreach($manifest["entries"] as $i => $part) {
+				$fileKey = substr($part["url"], strpos($part["url"], '/', 5) + 1);
+				$filePath = $tmpFilePath . '_' . md5(str_replace('/', '_', $fileKey));
+				$s3Client->getObject(array(
+					'Bucket' => $fileInfo["s3Path"]["bucket"],
+					'Key'    => $fileKey,
+					'SaveAs' => $filePath
+				));
+				$targetS3->putObject([
+					'Bucket' => $targetBucket,
+					'Key' => $targetBasePath . str_replace('.', '/', $tableId) . '.part_' . $i . '.csv.gz',
+					'Body' => fopen($filePath, 'r'),
+				]);
+				$fs->remove($tmpFilePath);
+			}
+		} else {
+			$tmpFilePath = $this->getTmpDir() . "/" . uniqid('table');
+			$s3Client->getObject(array(
+				'Bucket' => $fileInfo["s3Path"]["bucket"],
+				'Key'    => $fileInfo["s3Path"]["key"],
+				'SaveAs' => $tmpFilePath
+			));
+
+			$targetS3->putObject([
+				'Bucket' => $targetBucket,
+				'Key' => $targetBasePath . str_replace('.', '/', $tableId) . '.csv.gz',
+				'Body' => fopen($tmpFilePath, 'r'),
+			]);
+			$fs->remove($tmpFilePath);
 		}
 	}
 
