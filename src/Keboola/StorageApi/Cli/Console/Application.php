68457f1aafdf6ae2fb9aa968c071f808b1d9d3ab
Odin <odinuv@gmail.com>
2017-07-04 08:46:07 +0200

    fix: add command

--- src/Keboola/StorageApi/Cli/Console/Application.php
+++ src/Keboola/StorageApi/Cli/Console/Application.php
@@ -117,6 +117,7 @@ class Application extends BaseApplication
             new Command\DeleteTable(),
             new Command\WriteTable(),
             new Command\CopyTable(),
+            new Command\CreateBucket(),
             new Command\RestoreTableFromImports(),
             new Command\TruncateTable(),
             new Command\ListEvents(),
