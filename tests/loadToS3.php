4be2501960661c37fde0608f5dbda9d80d54f859
Ondřej Hlaváček <ondrej.hlavacek@keboola.com>
2017-07-05 13:21:47 -0700

    fix(tests): upload fixtures for RestoreProjectTest to S3 separately (#19)

---
+++ tests/loadToS3.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Loads test fixtures into S3
+ */
+
+date_default_timezone_set('Europe/Prague');
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+$basedir = dirname(__FILE__);
+
+require_once $basedir . '/../bootstrap.php';
+
+echo "Loading fixtures to S3\n";
+
+// delete from S3
+$s3Client = new \Aws\S3\S3Client([
+    'version' => 'latest',
+    'region' => TEST_AWS_REGION,
+    'credentials' => [
+        'key' => TEST_AWS_ACCESS_KEY_ID,
+        'secret' => TEST_AWS_SECRET_ACCESS_KEY,
+    ]
+]);
+$s3Client->deleteMatchingObjects(TEST_S3_BUCKET, '/cli-client-restore-test/');
+
+// Where the files will be source from
+$source = $basedir . '/data/backups';
+
+// Where the files will be transferred to
+$dest = 's3://' . TEST_S3_BUCKET . '/cli-client-restore-test/';
+
+
+// Create a transfer object.
+$manager = new \Aws\S3\Transfer($s3Client, $source, $dest, []);
+
+// Perform the transfer synchronously.
+$manager->transfer();
