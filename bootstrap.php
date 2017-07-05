51f37936cb0932f50e7232b919a6cffc7fa09c94
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-07-05 08:44:39 -0700

    fix(tests): upload s3 fixtures separately
    FIXES #18

--- bootstrap.php
+++ bootstrap.php
@@ -2,9 +2,9 @@
 // Define path to application directory
 define('ROOT_PATH', __DIR__);
 
-if (file_exists(__DIR__ . '/config.php')) {
+//if (file_exists(__DIR__ . '/config.php ')) {
     require_once __DIR__ . '/config.php';
-}
+//}
 
 require_once ROOT_PATH . '/vendor/autoload.php';
 
