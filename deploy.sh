7cbbc9c7aee43a89228d373b5b8f11460a774297
Odin <odinuv@gmail.com>
2017-07-11 10:08:57 +0200

    fix: use entrypoint fix: finish build matrix

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,3 @@ docker tag keboola/storage-api-cli quay.io/keboola/storage-api-cli:latest
 docker images
 docker push quay.io/keboola/storage-api-cli:${TRAVIS_TAG}
 docker push quay.io/keboola/storage-api-cli:latest
-
-TODO OPRAVIT OPRAVNENI
