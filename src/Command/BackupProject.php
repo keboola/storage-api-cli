9aa501ecd67f684c3176460a0914a08f9431bca1
Erik Zigo <erik.zigo@keboola.com>
2018-09-03 16:18:48 +0200

    backup-project command uses kbc-project-backup library

--- src/Command/BackupProject.php
+++ src/Command/BackupProject.php
@@ -3,20 +3,16 @@
 namespace Keboola\StorageApi\Cli\Command;
 
 use Aws\S3\S3Client;
-use Keboola\StorageApi\Client;
-use Keboola\StorageApi\HandlerStack;
-use Keboola\StorageApi\Options\GetFileOptions;
-use Keboola\Temp\Temp;
+use Keboola\ProjectBackup\S3Backup;
+use Psr\Log\LogLevel;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Filesystem\Filesystem;
 
 class BackupProject extends Command
 {
-    private const VERSION_LIMIT = 2;
-
     public function configure(): void
     {
         $this
@@ -34,10 +30,14 @@ class BackupProject extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
+        $sapiClient = $this->getSapiClient();
+        $logger = $this->getLogger($output);
+
         $s3 = new S3Client([
             'version' => 'latest',
             'region' => $input->getArgument('region'),
         ]);
+
         $bucket = $input->getArgument('bucket');
         $basePath = $input->getArgument('path');
         if ($basePath === '' || $basePath === '/') {
@@ -46,178 +46,38 @@ class BackupProject extends Command
             $basePath = rtrim($basePath, '/') . '/';
         }
 
-        $sapiClient = $this->getSapiClient();
+        $backup = new S3Backup($sapiClient, $s3, $logger);
 
-        $tables = $sapiClient->listTables(null, [
-            'include' => 'attributes,columns,buckets,metadata,columnMetadata',
-        ]);
-        $output->write("Exporting tables\n");
-        $s3->putObject([
-            'Bucket' => $bucket,
-            'Key' => $basePath . 'tables.json',
-            'Body' => json_encode($tables),
-        ]);
+        $backup->backupTablesMetadata($bucket, $basePath);
+        $backup->backupConfigs($bucket, $basePath, $input->getOption('include-versions'));
 
-        $output->write("Exporting buckets\n");
-        $s3->putObject([
-            'Bucket' => $bucket,
-            'Key' => $basePath . 'buckets.json',
-            'Body' => json_encode($sapiClient->listBuckets(["include" => "attributes,metadata"])),
-        ]);
-
-        $output->write("Exporting configurations\n");
-        $this->exportConfigs($sapiClient, $s3, $bucket, $basePath, $input->getOption('include-versions'));
-
-        $tablesCount = count($tables);
-        usort($tables, function ($a, $b) {
-            return strcmp($a["id"], $b["id"]);
-        });
-        $onlyStructure = $input->getOption('structure-only');
-        foreach (array_values($tables) as $i => $table) {
-            $currentTable = $i + 1;
-
-            if ($onlyStructure && $table['bucket']['stage'] !== 'sys') {
-                $output->write("Skipping table $currentTable/$tablesCount - {$table['id']} (sys bucket)\n");
-            } elseif (!$table['isAlias']) {
-                $output->write("Exporting table $currentTable/$tablesCount - {$table['id']}\n");
-                $this->exportTable($table['id'], $s3, $bucket, $basePath);
-            } else {
-                $output->write("Skipping table $currentTable/$tablesCount - {$table['id']} (alias)\n");
-            }
-        }
-    }
-
-    private function exportConfigs(Client $sapiClient, S3Client $s3, string $targetBucket, string $targetBasePath, bool $saveVersions): void
-    {
-        $limit = self::VERSION_LIMIT;
-        $tmp = new Temp();
-        $tmp->initRunFolder();
-
-        $configurationsFile = $tmp->createFile("configurations.json");
-        $versionsFile = $tmp->createFile("versions.json");
-
-        // use raw api call to prevent parsing json - preserve empty JSON objects
-        $sapiClient->apiGet('storage/components?include=configuration', $configurationsFile->getPathname());
-        $handle = fopen($configurationsFile, "r");
-        $s3->putObject([
-            'Bucket' => $targetBucket,
-            'Key' => $targetBasePath . 'configurations.json',
-            'Body' => $handle,
-        ]);
-        fclose($handle);
-
-        $url = "storage/components";
-        $url .= "?include=configuration,rows,state";
-        $sapiClient->apiGet($url, $configurationsFile->getPathname());
-        $configurations = json_decode(file_get_contents($configurationsFile->getPathname()));
-
-        foreach ($configurations as $component) {
-            foreach ($component->configurations as $configuration) {
-                if ($saveVersions) {
-                    $offset = 0;
-                    $versions = [];
-                    do {
-                        $url = "storage/components/{$component->id}/configs/{$configuration->id}/versions";
-                        $url .= "?include=name,description,configuration,state";
-                        $url .= "&limit={$limit}&offset={$offset}";
-                        $sapiClient->apiGet($url, $versionsFile->getPathname());
-                        $versionsTmp = json_decode(file_get_contents($versionsFile->getPathname()));
-                        $versions = array_merge($versions, $versionsTmp);
-                        $offset = $offset + $limit;
-                    } while (count($versionsTmp) > 0);
-                    $configuration->_versions = $versions;
-                }
-                if ($saveVersions) {
-                    foreach ($configuration->rows as &$row) {
-                        $offset = 0;
-                        $versions = [];
-                        do {
-                            $url = "storage/components/{$component->id}/configs/{$configuration->id}/rows/{$row->id}/versions";
-                            $url .= "?include=configuration";
-                            $url .= "&limit={$limit}&offset={$offset}";
-                            $sapiClient->apiGet($url, $versionsFile->getPathname());
-                            $versionsTmp = json_decode(file_get_contents($versionsFile->getPathname()));
-                            $versions = array_merge($versions, $versionsTmp);
-                            $offset = $offset + $limit;
-                        } while (count($versionsTmp) > 0);
-                        $row->_versions = $versions;
-                    }
-                }
-                $s3->putObject([
-                    'Bucket' => $targetBucket,
-                    'Key' => $targetBasePath . 'configurations/' . $component->id . '/' .
-                        $configuration->id . '.json',
-                    'Body' => json_encode($configuration),
-                ]);
-            }
-        }
-    }
-
-
-    private function exportTable(string $tableId, S3Client $targetS3, string $targetBucket, string $targetBasePath): void
-    {
-        $client = $this->getSapiClient();
-        $fileId = $client->exportTableAsync($tableId, [
-            'gzip' => true,
-        ]);
-        $fileInfo = $client->getFile($fileId["file"]["id"], (new GetFileOptions())->setFederationToken(true));
-
-        // Initialize S3Client with credentials from Storage API
-        $s3Client = new S3Client([
-            "version" => "latest",
-            "region" => $fileInfo["region"],
-            "credentials" => [
-                "key" => $fileInfo["credentials"]["AccessKeyId"],
-                "secret" => $fileInfo["credentials"]["SecretAccessKey"],
-                "token" => $fileInfo["credentials"]["SessionToken"],
-            ],
-        ]);
-
-        $fs = new Filesystem();
-        if ($fileInfo['isSliced'] === true) {
-            // Download manifest with all sliced files
-            $client = new \GuzzleHttp\Client([
-                'handler' => HandlerStack::create([
-                    'backoffMaxTries' => 10,
-                ]),
-            ]);
-            $manifest = json_decode($client->get($fileInfo['url'])->getBody(), true);
-
-            // Download all slices
-            $tmpFilePath = $this->getTmpDir() . DIRECTORY_SEPARATOR . uniqid('sapi-export-');
-            foreach ($manifest["entries"] as $i => $part) {
-                $fileKey = substr($part["url"], strpos($part["url"], '/', 5) + 1);
-                $filePath = $tmpFilePath . '_' . md5(str_replace('/', '_', $fileKey));
-                $s3Client->getObject(array(
-                    'Bucket' => $fileInfo["s3Path"]["bucket"],
-                    'Key' => $fileKey,
-                    'SaveAs' => $filePath
-                ));
-                $fh = fopen($filePath, 'r');
-                $targetS3->putObject([
-                    'Bucket' => $targetBucket,
-                    'Key' => $targetBasePath . str_replace('.', '/', $tableId) . '.part_' . $i . '.csv.gz',
-                    'Body' => $fh,
-                ]);
-                fclose($fh);
-                $fs->remove($filePath);
-            }
+        if ($input->getOption('structure-only')) {
+            $logger->warning('Skipping exporting tables data (structure-only export)');
         } else {
-            $tmpFilePath = $this->getTmpDir() . DIRECTORY_SEPARATOR . uniqid('table');
-            $s3Client->getObject(array(
-                'Bucket' => $fileInfo["s3Path"]["bucket"],
-                'Key' => $fileInfo["s3Path"]["key"],
-                'SaveAs' => $tmpFilePath
-            ));
+            $tables = $sapiClient->listTables(null);
 
-            $fh = fopen($tmpFilePath, 'r');
-            $targetS3->putObject([
-                'Bucket' => $targetBucket,
-                'Key' => $targetBasePath . str_replace('.', '/', $tableId) . '.csv.gz',
-                'Body' => $fh,
-            ]);
-            fclose($fh);
-            $fs->remove($tmpFilePath);
+            $tablesCount = count($tables);
+            usort($tables, function ($a, $b) {
+                return strcmp($a["id"], $b["id"]);
+            });
+
+            foreach ($tables as $i => $table) {
+                $output->writeln(sprintf('Table %d/%d', $i + 1, $tablesCount));
+                $backup->backupTable($table['id'], $bucket, $basePath);
+            }
         }
+    }
+
+    private function getLogger(OutputInterface $output): ConsoleLogger
+    {
+        return new ConsoleLogger(
+            $output,
+            [
+                LogLevel::INFO => OutputInterface::VERBOSITY_NORMAL,
+            ],
+            [
+                LogLevel::WARNING => 'comment',
+            ]
+        );
     }
 }
